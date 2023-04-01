7f6cde60850d803d80721d781c8cd219bbff2527
Giyo Santana Dias <giosantdias@gmail.com>
2023-04-01 01:00:44 -0300

    loja de games

---
+++ src/main/java/org/generation/lojadegames/model/Usuario.java
@@ -0,0 +1,98 @@
+package org.generation.lojadegames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Entity
+@Table(name= "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O nome não pode ser vazio.")
+	@Size(max = 50, message = "O nome pode ter até 60 caracteres.")
+	private String nome;
+	
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O usuário não pode ser vazio.")
+	@Email(message= "O usuário deve ser um e-mail válido.")
+	private String usuario;
+	
+	@NotBlank(message = "A senha não pode ser vazio.")
+	@Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres.")
+	private String senha;
+	
+	@Size(max = 5000, message= "O link da foto deve ter no máximo 5000 caracteres.")
+	private String foto;
+	
+	@OneToMany(mappedBy= "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+
+}
